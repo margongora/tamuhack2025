362b5188335afca72adade7b5171211e1fd735ac
Noah Garcia <noah@danielgarcia.info>
2025-01-25 16:49:44 -0600

    polyline3d and marker3d support

--- tamuhack25/src/app/page.tsx
+++ tamuhack25/src/app/page.tsx
@@ -1,14 +1,21 @@
 import AirportForm from "@/components/AirportForm";
-import { Map3D } from "@/components/map-3d";
+import { Map3D, Marker3D, Polyline3D } from "@/components/map-3d";
 
 export default function Home() {
-
-
-
   return (
     <div className="w-screen h-screen dark">
       <AirportForm />
-      <Map3D center={{ lat: 30.267, lng: -97.7431, altitude: 5000 }} />
+      <Map3D center={{ lat: 30.267, lng: -97.7431, altitude: 5000 }}>
+        <Polyline3D altitudeMode={'RELATIVE_TO_GROUND'} coordinates={[
+            {lat: 32.7767, lng: -96.7970, altitude: 30000},
+            {lat: 40.7555, lng:  -73.9739, altitude: 30000},
+          ]}
+          strokeColor="#500000" strokeWidth={10}
+          geodesic
+          drawsOccludedSegments
+          ></Polyline3D>
+          <Marker3D position={{lat: 32.7767, lng: -96}}></Marker3D>
+      </Map3D>
     </div>
   );
 }
--- tamuhack25/src/components/map-3d/map-3d.tsx
+++ tamuhack25/src/components/map-3d/map-3d.tsx
@@ -4,6 +4,7 @@ import { useMapsLibrary } from '@vis.gl/react-google-maps';
 import React, {
   ForwardedRef,
   forwardRef,
+  ReactNode,
   useEffect,
   useImperativeHandle,
   useState
@@ -16,6 +17,7 @@ import { getAllAirports } from '@/lib/client/utils';
 
 export type Map3DProps = google.maps.maps3d.Map3DElementOptions & {
   onCameraChange?: (cameraProps: Map3DCameraProps) => void;
+  children?: ReactNode;
 };
 
 export type Map3DCameraProps = {
@@ -25,6 +27,86 @@ export type Map3DCameraProps = {
   tilt: number;
   roll: number;
 };
+
+export type Polyline3DProps = {
+  altitudeMode: string;
+  coordinates: google.maps.LatLngAltitudeLiteral[];
+  drawsOccludedSegments?: boolean;
+  extruded?: boolean;
+  geodesic?: boolean;
+  outerColor?: string;
+  outerWidth?: number;
+  strokeColor?: string;
+  strokeWidth?: number;
+  zIndex?: number;
+}
+
+export type Marker3DProps = {
+  altitudeMode?: string;
+  position: google.maps.LatLngLiteral;
+}
+
+export const Polyline3D = forwardRef(
+  (
+    props: Polyline3DProps,
+    forwardedRef: ForwardedRef<google.maps.maps3d.Polyline3DElement | null>
+  ) => {
+    useMapsLibrary('maps3d');
+
+    const [polyline3DElement, polyline3dRef] = useCallbackRef<google.maps.maps3d.Polyline3DElement>();
+
+    useEffect(() => {
+      customElements.whenDefined('gmp-map-3d').then(() => {
+        setCustomElementsReady(true);
+      });
+    }, []);
+
+    const [customElementsReady, setCustomElementsReady] = useState(false);
+
+    useImperativeHandle<
+      google.maps.maps3d.Polyline3DElement | null,
+      google.maps.maps3d.Polyline3DElement | null
+    >(forwardedRef, () => polyline3DElement, [polyline3DElement]);
+
+    if (!customElementsReady) return null;
+    
+    return (
+      <>
+        <gmp-polyline-3d
+          ref={polyline3dRef}
+          {...props}
+        >
+
+        </gmp-polyline-3d>
+      </>
+    )
+  }
+);
+
+export const Marker3D = (
+    props: Marker3DProps,
+  ) => {
+    useMapsLibrary('maps3d');
+
+    useEffect(() => {
+      customElements.whenDefined('gmp-marker-3d').then(() => {
+        setCustomElementsReady(true);
+      });
+    }, []);
+
+    const [customElementsReady, setCustomElementsReady] = useState(false);
+
+    if (!customElementsReady) return null;
+    
+    return (
+      <>
+        <gmp-marker-3d
+        {...props}
+        >          
+        </gmp-marker-3d>
+      </>
+    )
+  };
 
 export const Map3D = forwardRef(
   (
@@ -52,8 +134,7 @@ export const Map3D = forwardRef(
       // logic
       getAllAirports().then((flights) => {
         // make the markers here
-        
-
+      
       }).catch((error) => {
         console.error(error);
       });
@@ -62,7 +143,7 @@ export const Map3D = forwardRef(
 
     useEffect(() => {
 
-      customElements.whenDefined('gmp-map-3d').then(() => {
+      customElements.whenDefined('gmp-polyline-3d').then(() => {
         setCustomElementsReady(true);
       });
       if (navigator.geolocation) {
@@ -83,7 +164,7 @@ export const Map3D = forwardRef(
       }
     }, []);
 
-    const { center, heading, tilt, range, roll, ...map3dOptions } = props;
+    const { center, heading, tilt, range, roll, children, ...map3dOptions } = props;
 
     useDeepCompareEffect(() => {
       if (!map3DElement) return;
@@ -110,6 +191,7 @@ export const Map3D = forwardRef(
           heading={heading}
           tilt={tilt}
           roll={roll}>
+            {props.children}
         </gmp-map-3d>
 
       </>
