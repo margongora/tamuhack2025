6e11d6aff945ff324eb43d7b941048d081e23198
margongora <margongora0803@gmail.com>
2025-01-25 16:52:48 -0600

    Got date change to work

--- tamuhack25/src/app/page.tsx
+++ tamuhack25/src/app/page.tsx
@@ -1,13 +1,58 @@
-import AirportForm from "@/components/AirportForm";
+'use client'
+import {
+  Button,
+  DatePicker,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  Form,
+  useDisclosure
+} from '@heroui/react'
 import { Map3D } from "@/components/map-3d";
+import { useEffect, useState } from 'react';
 
 export default function Home() {
 
+  const [date, setDate] = useState<string>('');
 
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // Prevent default browser page refresh.
+    e.preventDefault();
+
+    // Get form data as an object.
+    const data = Object.fromEntries(new FormData(e.currentTarget));
+    setDate(data['leaveDate'] as string);
+  };
+
+  useEffect(() => {
+    if (date !== '') {
+      console.log('date changed')
+    }
+  }, [date])
 
   return (
     <div className="w-screen h-screen dark">
-      <AirportForm />
+      <div className='dark'>
+        <p className='text-white'>{date}</p>
+        <Button onPress={onOpen}>Input Info</Button>
+        <Drawer backdrop='blur' isOpen={isOpen} onOpenChange={onOpenChange} placement="left">
+          <DrawerContent className='bg-gray-400'>
+            {(onClose) => (
+              <>
+                <DrawerHeader>Input what date you&apos;d like to leave.</DrawerHeader>
+                <DrawerBody>
+                  <Form onSubmit={onSubmit}>
+                    <DatePicker name='leaveDate' className='text-black' />
+                    <Button type='submit' onPress={onClose}>Submit</Button>
+                  </Form>
+                </DrawerBody>
+              </>
+            )}
+          </DrawerContent>
+        </Drawer>
+      </div>
       <Map3D center={{ lat: 30.267, lng: -97.7431, altitude: 5000 }} />
     </div>
   );
--- tamuhack25/src/components/AirportForm.tsx
+++ tamuhack25/src/components/AirportForm.tsx
@@ -1,37 +1,46 @@
-'use client'
-import {
-	Button,
-	Drawer,
-	DrawerBody,
-	DrawerContent,
-	DrawerFooter,
-	DrawerHeader,
-	useDisclosure
-} from '@heroui/react'
+// 'use client'
+// import {
+// 	Button,
+// 	DatePicker,
+// 	Drawer,
+// 	DrawerBody,
+// 	DrawerContent,
+// 	DrawerHeader,
+// 	Form,
+// 	useDisclosure
+// } from '@heroui/react'
 
-const AirportForm = () => {
-	const { isOpen, onOpen, onOpenChange } = useDisclosure();
+// const AirportForm = () => {
+// 	const { isOpen, onOpen, onOpenChange } = useDisclosure();
+// 	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+// 		// Prevent default browser page refresh.
+// 		e.preventDefault();
 
-	return (
-		<div className='dark'>
-			<Button onPress={onOpen}>Input Info</Button>
-			<Drawer backdrop='blur' isOpen={isOpen} onOpenChange={onOpenChange} placement="left">
-				<DrawerContent className='bg-gray-400'>
-					{(onClose) => (
-						<>
-							<DrawerHeader>Input what date you&apos;d like to leave.</DrawerHeader>
-							<DrawerBody>
-								<p>FUCK</p>
-							</DrawerBody>
-							<DrawerFooter>
-								<Button color="success" onPress={onClose}>Submit</Button>
-							</DrawerFooter>
-						</>
-					)}
-				</DrawerContent>
-			</Drawer>
-		</div>
-	)
-}
+// 		// Get form data as an object.
+// 		const data = Object.fromEntries(new FormData(e.currentTarget));
+// 		console.log(data);
+// 	};
 
-export default AirportForm
+// 	return (
+// 		<div className='dark'>
+// 			<Button onPress={onOpen}>Input Info</Button>
+// 			<Drawer backdrop='blur' isOpen={isOpen} onOpenChange={onOpenChange} placement="left">
+// 				<DrawerContent className='bg-gray-400'>
+// 					{(onClose) => (
+// 						<>
+// 							<DrawerHeader>Input what date you&apos;d like to leave.</DrawerHeader>
+// 							<DrawerBody>
+// 								<Form onSubmit={onSubmit}>
+// 									<DatePicker name='leaveDate' className='text-black' />
+// 									<Button type='submit' onPress={onClose}>Submit</Button>
+// 								</Form>
+// 							</DrawerBody>
+// 						</>
+// 					)}
+// 				</DrawerContent>
+// 			</Drawer>
+// 		</div>
+// 	)
+// }
+
+// export default AirportForm
